22dab52c6c97be62b2c100b850b35bc1545c09c2
inwookjung <inwook.jung@kr.doubledown.com>
2023-01-22 18:42:56 +0900

    first commit

---
+++ src/main/java/com/inwook/springbootdeploy/CodedeployApplication.java
@@ -0,0 +1,13 @@
+package com.inwook.springbootdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodedeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodedeployApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inwook/springbootdeploy/CodedeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inwook.springbootdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodedeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
